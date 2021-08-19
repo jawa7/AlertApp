ecfc21df842e36e8e1ae6e357b8265e0a4810134
Ilya <jawa009@gmail.com>
2021-08-19 17:05:21 +0300

    complete

--- app/src/main/java/com/example/alertapp/MyLifecycleOwner.kt
+++ app/src/main/java/com/example/alertapp/MyLifecycleOwner.kt
@@ -11,15 +11,9 @@ internal class MyLifecycleOwner: SavedStateRegistryOwner {
     private var mLifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
     private var mSavedStateRegistryController: SavedStateRegistryController = SavedStateRegistryController.create(this)
 
-    val isInitialized: Boolean
-    get() = true
 
     override fun getLifecycle(): Lifecycle {
         return mLifecycleRegistry
-    }
-
-    fun setCurrentState(state: Lifecycle.State) {
-        mLifecycleRegistry.currentState = state
     }
 
     fun handleLifecycleEvent(event: Lifecycle.Event) {
@@ -32,9 +26,5 @@ internal class MyLifecycleOwner: SavedStateRegistryOwner {
 
     fun performRestore(savedState: Bundle?) {
         mSavedStateRegistryController.performRestore(savedState)
-    }
-
-    fun performSave(outBundle: Bundle) {
-        mSavedStateRegistryController.performRestore(outBundle)
     }
 }
--- app/src/main/java/com/example/alertapp/OverlayShowingService.kt
+++ app/src/main/java/com/example/alertapp/OverlayShowingService.kt
@@ -4,152 +4,143 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.os.IBinder
 import android.view.*
-import android.widget.Toast
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import android.view.View.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Stop
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.consumeAllChanges
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.lifecycle.ViewTreeViewModelStoreOwner
 import androidx.savedstate.ViewTreeSavedStateRegistryOwner
-import kotlin.math.roundToInt
 
 class OverlayShowingService : Service() {
 
-//    private lateinit var topLeftView: View
-//    private var offsetX: Float = 0.0f
-//    private var offsetY: Float = 0.0f
-//    private var originalXPos: Int = 0
-//    private var originalYPos: Int = 0
-//    private var moving: Boolean = false
     private lateinit var windowManager: WindowManager
 
-    private lateinit var composeView: ComposeView
+    private lateinit var playButton: ComposeView
+
+    private lateinit var stopButton: ComposeView
+
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate() {
         super.onCreate()
         windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
+        // Initializing Compose
+        playButton = ComposeView(this)
 
+        stopButton = ComposeView(this)
+
+        // Writing parameters for buttons for View system
         val params: WindowManager.LayoutParams =
             WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT
             )
         params.gravity = Gravity.START or Gravity.TOP
         params.x = 0
         params.y = 0
 
-        composeView = ComposeView(this)
-        composeView.setContent {
-//            var offsetX by remember { mutableStateOf(0f) }
-//            var offsetY by remember { mutableStateOf(0f) }
-            IconButton(
-                onClick = { Toast.makeText(this, "Overlay button click event", Toast.LENGTH_SHORT).show() },
+        // Creating ringtone manager and media player for the signal to sound
+        val alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val mMediaPlayer = MediaPlayer()
+        mMediaPlayer.setDataSource(this, alert)
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        if (audioManager.getStreamVolume(AudioManager.STREAM_RING) != 0 && !mMediaPlayer.isPlaying) {
+            mMediaPlayer.setAudioAttributes(
+                AudioAttributes
+                    .Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build()
+            )
+            // isLooping true for loop the sound
+            mMediaPlayer.isLooping = true
+        }
+
+        // We create both buttons, but we want to display only at the start - playButton
+        stopButton.visibility = GONE
+
+        playButton.setContent {
+            Icon(
+                imageVector = Icons.Default.PlayArrow,
+                contentDescription = "",
                 modifier = Modifier
-//                    .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
-//                    .pointerInput(MainActivity()) {
-//                        detectDragGestures { change, dragAmount ->
-//                            change.consumeAllChanges()
-//                            offsetX += dragAmount.x
-//                            offsetY += dragAmount.y
-//                        }
-//                    }
-                    .padding(16.dp)
-                    .size(24.dp)
+                    .combinedClickable(
+                    onClick = {
+                        // When clicking on playButton starts the sound and playButton changes to stopButton
+                        mMediaPlayer.prepare()
+                        mMediaPlayer.start()
+                        playButton.visibility = GONE
+                        stopButton.visibility = VISIBLE
+                    },
+                    onLongClick = {
+                        // If it is long press playButton and stopButton will be deleted
+                        windowManager.removeViewImmediate(playButton)
+                        windowManager.removeViewImmediate(stopButton)
+                    }
+                )
+                    .padding(12.dp),
+                tint = Color.Green
+            )
+        }
+
+
+        stopButton.setContent {
+            IconButton(
+                onClick = {
+                    mMediaPlayer.stop()
+                    stopButton.visibility = GONE
+                    playButton.visibility = VISIBLE
+                }
             ) {
                 Icon(
-                    Icons.Default.PlayArrow,
+                    imageVector = Icons.Default.Stop,
                     contentDescription = "",
-                    modifier = Modifier,
-                    tint = androidx.compose.ui.graphics.Color.Green
+                    tint = Color.Red,
+                    modifier = Modifier
                 )
             }
         }
+        // Initializing lifecycle for Compose
+        lifeCycleCompose(playButton)
+        lifeCycleCompose(stopButton)
 
+        windowManager.addView(stopButton, params)
+        windowManager.addView(playButton, params)
+    }
+
+    private fun lifeCycleCompose(v: View) {
+        //Trick the ComposeView into thinking we are tracking lifecycle
         val lifecycleOwner = MyLifecycleOwner()
         val viewModelStore = ViewModelStore()
         lifecycleOwner.performRestore(null)
         lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        ViewTreeLifecycleOwner.set(composeView, lifecycleOwner)
-        ViewTreeViewModelStoreOwner.set(composeView)
+        ViewTreeLifecycleOwner.set(v, lifecycleOwner)
+        ViewTreeViewModelStoreOwner.set(v)
         { viewModelStore }
-        ViewTreeSavedStateRegistryOwner.set(composeView, lifecycleOwner)
-
-        windowManager.addView(composeView, params)
+        ViewTreeSavedStateRegistryOwner.set(v, lifecycleOwner)
     }
-
-
-//    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-//        if (event?.action == MotionEvent.ACTION_DOWN) {
-//            val x: Float = event.rawX
-//            val y: Float = event.rawY
-//
-//            moving = false
-//
-//            val location: MutableList<Int> = mutableListOf()
-//
-//            originalXPos = location[0]
-//            originalYPos = location[1]
-//
-//            offsetX = originalXPos - x
-//            offsetY = originalYPos - y
-//        } else if (event?.action == MotionEvent.ACTION_MOVE) {
-//            val topLeftLocationOnScreen = IntArray(2)
-//            topLeftView.getLocationOnScreen(topLeftLocationOnScreen)
-//
-//            println("topLeftLocation = " + topLeftLocationOnScreen[1])
-//            println("originalY = $originalYPos")
-//
-//            val x: Float = event.rawX
-//            val y: Float = event.rawY
-//
-//            //val params = composeView.layoutParams as WindowManager.LayoutParams
-//
-//            val newX: Int = (offsetX + x).roundToInt()
-//            val newY: Int = (offsetY + y).roundToInt()
-//
-//            if (Math.abs(newX - originalXPos) < 1 && abs(newY - originalYPos) < 1 && !moving) {
-//                return false
-//            }
-//
-//            // params.x = newX - (topLeftLocationOnScreen[0])
-//            // params.y = newY - (topLeftLocationOnScreen[1])
-//
-//            // windowManager.updateViewLayout(composeView, params)
-//            moving = true
-//        } else if (event?.action == MotionEvent.ACTION_UP) {
-//            if (moving) {
-//                return true
-//            }
-//        }
-//        return false
-//    }
-
-//    override fun onClick(v: View?) {
-//        Toast.makeText(this, "Overlay button click event", Toast.LENGTH_SHORT).show()
-//    }
 }
